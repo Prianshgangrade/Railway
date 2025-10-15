3b2ed68f6e2ae74d6e2d98ba54dedf4b00b5fccc
Priansh Gangrade <gangradepriansh@gmail.com>
2025-10-15 10:50:59 +0530

    try1

--- api/vercel_handler.py
+++
@@ -1,6 +0,0 @@
-from index import app
-
-# This exposes your Flask app to Vercel
-# DO NOT run app.run() — Vercel will handle requests
-def handler(request, context):
-    return app(request.environ, lambda status, headers: None)
